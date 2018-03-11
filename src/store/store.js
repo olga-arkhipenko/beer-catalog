3a31003007d7e469138a81147908dba81da34e6f
Olga Arkhipenko <orangeorca909@gmail.com>
2018-03-12 00:58:09 +0300

    Added vuex store.

---
+++ src/store/store.js
@@ -0,0 +1,26 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import api from '/Users/arkhipenko.o/Documents/dev/js/iTechArt/beer-catalog/src/api/api.js'
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+    state: {
+        beers: []
+    },
+    getters: {
+        getBeersForCatalog(state) {
+            return state.beers.map(beer => ({id: beer.id, name: beer.name, image: beer.image_url, tagline: beer.tagline}));
+        }
+    },
+    mutations: {
+        SET_BEERS(state, beers){
+            state.beers.push(...beers);
+        }
+    },
+    actions: {
+        getBeerPage({commit}, page, perPage) {
+            api.get(page, perPage).then(beers => commit('SET_BEERS', JSON.parse(beers)));
+        }
+    }
+})
