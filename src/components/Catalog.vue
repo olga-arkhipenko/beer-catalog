41b0777a6da250bcfee87052625a45ba4957103a
Olga Arkhipenko <orangeorca909@gmail.com>
2018-03-12 01:01:43 +0300

    Added beers in catalog component.

--- src/components/Catalog.vue
+++ src/components/Catalog.vue
@@ -1,5 +1,45 @@
 <template>
-    <div>
-        Hello from Catalog
-    </div>
+    <section class="catalog">
+        <div v-for="beer in beers">
+            <img :src="beer.image" alt="Beer pic">
+            <h2>{{beer.name}}</h2>
+            <h3>{{beer.tagline}}</h3>
+        </div>
+    </section>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            currentPage: 1,
+        }
+    },
+    created() {
+        if(this.currentPage > 1) {
+            return;
+        }
+        this.loadBeers(this.currentPage);
+    },
+    computed:{
+        beers() {
+            return this.$store.getters.getBeersForCatalog;
+        }
+    },
+    methods: {
+        loadBeers(){
+            this.$store.dispatch('getBeerPage', this.currentPage);
+            this.currentPage++;
+        },
+        touchBottom(){
+            const scrollY = window.scrollY
+            const visible = document.documentElement.clientHeight
+            const pageHeight = document.documentElement.scrollHeight
+            const pageBottom = visible + scrollY >= pageHeight
+            return pageBottom || pageHeight < visible
+        }
+    }
+  
+}
+</script>
+
