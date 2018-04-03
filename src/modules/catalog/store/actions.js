7ab452a7d37d169baaa81fbbf35af09011707883
Olga Arkhipenko <volha.arkhipenka@itechart-group.com>
2018-04-03 15:20:37 +0300

    Moved common/helpers/utilities to common/utilities.

--- src/modules/catalog/store/actions.js
+++ src/modules/catalog/store/actions.js
@@ -3,7 +3,6 @@ import catalogService from '../services/catalogService';
 export default {
     loadBeers({ commit }, beerParams) {
         catalogService.fetchBeers(beerParams).then((beers) => {
-            console.log(JSON.stringify(beers));
             commit('pushBeers', beers);
         });
     },
