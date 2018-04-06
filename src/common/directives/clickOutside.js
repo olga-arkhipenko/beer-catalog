3234193363caf2af4dbab8b7c5331980ea5fb86a
Olga Arkhipenko <volha.arkhipenka@itechart-group.com>
2018-04-06 12:39:34 +0300

    Added click outside directive for menu.

--- src/common/directives/clickOutside.js
+++ src/common/directives/clickOutside.js
@@ -1,3 +1,18 @@
 export default {
+    isFn: true,
+    bind(el, binding) {
+        const handler = (e) => {
+            if ((!el.contains(e.target) && el !== e.target)) {
+                binding.value();
+            }
+        };
+        el.clickOutside = handler;
+        document.addEventListener('click', handler);
+    },
+
+    unbind(el) {
+        document.removeEventListener('click', el.clickOutside);
+        el.clickOutside = null;
+    }
 };
 
