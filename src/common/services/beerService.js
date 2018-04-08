4b6ec22ff70b1b1360195c3ef81debc847bb8584
Olga Arkhipenko <orangeorca909@gmail.com>
2018-04-09 01:19:17 +0300

    Refactored url creating methods.

--- src/common/services/beerService.js
+++ src/common/services/beerService.js
@@ -3,14 +3,12 @@ import urlMapper from 'common/utilities/urlMapper';
 import urlCreator from 'common/utilities/urlCreator';
 import { URL } from 'features/punkApiManagement/url';
 import { QUERY_PARAMS_MAP } from 'features/punkApiManagement/queryParamsMap';
-import { ROUTE_PARAMS_MAP } from 'features/punkApiManagement/routeParamsMap';
 import beerMapper from 'features/punkApiManagement/beerMapper';
 
 export default {
     fetchBeers(beerParams) {
-        const mappedParams = urlMapper.mapParams(QUERY_PARAMS_MAP, ROUTE_PARAMS_MAP, beerParams);
+        const mappedParams = urlMapper.mapParams(QUERY_PARAMS_MAP, beerParams);
         const url = urlCreator.create(URL, mappedParams);
-        console.log(url);
         return ajaxHelper.get(url).then(beers => beers.map(beer => beerMapper.mapToBeer(beer)));
     }
 };
--- src/modules/beerInfo/services/beerDetailsService.js
+++ src/modules/beerInfo/services/beerDetailsService.js
@@ -1,15 +1,11 @@
 import ajaxHelper from 'common/helpers/ajaxHelper';
-import urlMapper from 'common/utilities/urlMapper';
 import urlCreator from 'common/utilities/urlCreator';
 import { URL } from 'features/punkApiManagement/url';
-import { QUERY_PARAMS_MAP } from 'features/punkApiManagement/queryParamsMap';
-import { ROUTE_PARAMS_MAP } from 'features/punkApiManagement/routeParamsMap';
 import beerMapper from 'features/punkApiManagement/beerMapper';
 
 export default {
     fetchBeer(beerId) {
-        const mappedParams = urlMapper.mapParams(QUERY_PARAMS_MAP, ROUTE_PARAMS_MAP, { beerId });
-        const url = urlCreator.create(URL, mappedParams);
+        const url = urlCreator.create(URL, { id: beerId });
         console.log(url);
         return ajaxHelper.get(url).then((beer) => {
             beerMapper.mapToBeerDetails(beer[0]);
