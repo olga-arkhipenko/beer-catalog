7989a2d8be7bcdc024ec004be1267b68e775dd36
Olga Arkhipenko <orangeorca909@gmail.com>
2018-05-15 19:45:58 +0300

    Added single beer route.

--- src/server/view/routes/beers.js
+++ src/server/view/routes/beers.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const catalogController = require('../controllers/catalogController');
+const beerPageController = require('../controllers/beerPageController');
 
 const router = express.Router();
 
 router.get('/', catalogController.getBeers);
+router.get('/:id', beerPageController.getBeer);
 
 module.exports = router;
