6c88234ef2258f8b0f8db61e15bee96e88d20f05
Olga Arkhipenko <orangeorca909@gmail.com>
2018-05-15 19:04:03 +0300

    Added punk api data mapping.

--- src/server/view/controllers/catalogController.js
+++ src/server/view/controllers/catalogController.js
@@ -3,15 +3,16 @@ const paramsMapper = require('../../utils/paramsMapper');
 const urlCreator = require('../../utils/urlCreator');
 const URL = require('../../punkApiManagement/url');
 const requestHelper = require('../../helpers/requestHelper');
+const beerMapper = require('../../punkApiManagement/mappers/beerMapper');
 
 module.exports = {
     getBeers(req, res) {
         const mappedParams = paramsMapper.mapParams(QUERY_PARAMS_MAP, req.query);
         const url = urlCreator.create(URL, mappedParams);
         console.log(`url${url}`);
-        requestHelper.get(url, (data) => {
-            console.log(data);
-            res.json(data);
+        requestHelper.get(url, (beers) => {
+            const mappedBeers = beers.map(beerMapper.mapToBeer);
+            res.json(mappedBeers);
         });
     }
 };
