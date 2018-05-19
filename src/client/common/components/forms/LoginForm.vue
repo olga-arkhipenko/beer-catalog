c71a5844c2ddf98b09004e375f7feba18a4d71eb
Olga Arkhipenko <orangeorca909@gmail.com>
2018-05-20 01:05:52 +0300

    Added login form styles.

--- src/client/common/components/forms/LoginForm.vue
+++ src/client/common/components/forms/LoginForm.vue
@@ -1,11 +1,68 @@
 <template>
     <form
         action="post"
-        class="page">
-        Login
+        class="form"
+        @submit.prevent="submitLogin">
+        <label>
+            <p>Email</p>
+            <input
+                v-model.trim="email"
+                type="email"
+                required
+                placeholder="Enter your email"
+                size="28">
+        </label>
+        <label>
+            <p>Password</p>
+            <input
+                v-model="password"
+                type="password"
+                required
+                placeholder="Enter your password"
+                size="28">
+        </label>
+        <div class="form__button-wrapper">
+            <button
+                type="submit"
+                class="action-button login-button">Log in</button>
+        </div>
     </form>
 </template>
 
+<script>
+export default {
+    data() {
+        return {
+            login: '',
+            password: ''
+        };
+    },
+    methods: {
+        submitLogin() {
+            console.log('submit login');
+        }
+    }
+};
+</script>
+
+
 <style>
+.login-heading {
+    text-align: center;
+
+    color: #3fbd3e;
+
+    font-weight: 600;
+}
+
+.login-button {
+    text-align: center;
+    text-transform: uppercase;
+
+    color: #3fbd3e;
+
+    font-size: 1.8rem;
+    font-weight: 600;
+}
 
 </style>
