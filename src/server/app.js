82f1b1a080693d7e627e2cabe6fdbcde15f0cd48
Olga Arkhipenko <orangeorca909@gmail.com>
2018-05-20 15:28:07 +0300

    Replaced router.

--- src/server/app.js
+++ src/server/app.js
@@ -1,16 +1,19 @@
 const path = require('path');
 const express = require('express');
 const router = require('./view/routing/router');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-router(app);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(express.static('.'));
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../../index.html'));
 });
 
+router(app);
+
 
 app.listen(3030);
-
