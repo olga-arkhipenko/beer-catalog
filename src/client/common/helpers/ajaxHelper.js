903b67c8c35649b073ec46a5382b1ea487821a4d
Olga Arkhipenko <orangeorca909@gmail.com>
2018-05-20 15:26:32 +0300

    Added post method to ajax helper.

--- src/client/common/helpers/ajaxHelper.js
+++ src/client/common/helpers/ajaxHelper.js
@@ -3,5 +3,17 @@ export default {
         return fetch(url)
             .then(response => response.json())
             .catch(new Error('fetch denied'));
+    },
+    post(url, data) {
+        console.log('here');
+        return fetch(url, {
+            method: 'post',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include',
+            body: data
+        }).then(response => response.status);
     }
 };
