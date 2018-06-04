4976f7e60a25d5e978962de022763ce6978b201c
Olga Arkhipenko <orangeorca909@gmail.com>
2018-06-04 13:17:11 +0300

    Added image model to data layer.

--- server/dataAccess/models/Image.js
+++ server/dataAccess/models/Image.js
@@ -1,10 +1,13 @@
 module.exports = (sequelize, DataTypes) => {
     const image = sequelize.define('Image', {
-        name: {
-            type: DataTypes.STRING
-        },
         url: {
             type: DataTypes.STRING
+        },
+        width: {
+            type: DataTypes.INTEGER
+        },
+        height: {
+            type: DataTypes.INTEGER
         }
     });
 
