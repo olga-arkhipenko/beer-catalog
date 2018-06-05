365831eec0aecb146f8f30a929f248da9db35a47
Olga Arkhipenko <orangeorca909@gmail.com>
2018-06-05 19:54:37 +0300

    Modified constatnts.

--- client/common/api/constants/urls.js
+++ client/common/api/constants/urls.js
@@ -1,6 +1,6 @@
 export default {
     beers: '/api/beers',
-    imageUpload: '/ api / uploadImage',
+    imageUpload: '/api/uploadImage',
     registration: '/api/registration',
     login: '/api/login'
 };
