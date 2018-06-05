85bb108feb893769705786fd162268e0b9f4c5ca
Olga Arkhipenko <orangeorca909@gmail.com>
2018-06-05 19:55:24 +0300

    Added catalog notification.

--- client/common/services/beerService.js
+++ client/common/services/beerService.js
@@ -4,14 +4,20 @@ import urlCreator from 'common/utilities/urlCreator';
 import queryParamsMap from 'common/api/constants/queryParamsMap';
 import urls from 'common/api/constants/urls';
 import beerMapper from 'common/api/mappers/beerMapper';
+import notificationHelper from 'common/helpers/notification/notificationHelper';
+import errors from 'common/helpers/notification/errors';
 
 export default {
     fetchBeers(beerParams) {
         const mappedParams = urlMapper.mapParams(queryParamsMap, beerParams);
         const url = urlCreator.create(urls.beers, mappedParams);
-        return ajaxHelper
+        const promise = ajaxHelper
             .get(url)
             .then(beers => beers.map(beerMapper.mapToBeer));
-        // .catch(err => console.error(err));
+        promise.catch(() => {
+            notificationHelper.showError(errors.beersLoading);
+        });
+
+        return promise;
     }
 };
