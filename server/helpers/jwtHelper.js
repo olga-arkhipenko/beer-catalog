257af0bd0a1d072e69c569f9936b297767ab5dc7
Olga Arkhipenko <orangeorca909@gmail.com>
2018-06-05 12:48:55 +0300

    Separated jwt logic.

--- server/helpers/jwtHelper.js
+++ server/helpers/jwtHelper.js
@@ -1,6 +1,11 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = {
+    createToken(payload) {
+        return jwt.sign(payload, 'secret', {
+            expiresIn: 86400
+        });
+    },
     verifyToken(token) {
         return new Promise((resolve, reject) => {
             jwt.verify(token, 'secret', (err, decodedToken) => {
