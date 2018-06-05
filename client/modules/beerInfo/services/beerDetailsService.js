20a416919d7d2fdd6301cffd798d0d8da4dd8526
Olga Arkhipenko <orangeorca909@gmail.com>
2018-06-05 20:20:45 +0300

    Added single beer load error notification template.

--- client/modules/beerInfo/services/beerDetailsService.js
+++ client/modules/beerInfo/services/beerDetailsService.js
@@ -1,12 +1,16 @@
 import ajaxHelper from 'common/helpers/ajaxHelper';
 import beerMapper from 'common/api/mappers/beerMapper';
+import notificationHelper from 'common/helpers/notification/notificationHelper';
+import errors from 'common/helpers/notification/errors';
 
 export default {
     fetchBeer(beerId) {
         const url = `/api/beers/${beerId}`;
-        return ajaxHelper
+        const promise = ajaxHelper
             .get(url)
             .then(beer => beerMapper.mapToBeerDetails(beer));
-        // .catch(err => console.error(err));
+        promise.catch(() => notificationHelper.showError(errors.beerLoading));
+
+        return promise;
     }
 };
