131c39cbd125baaeb729df45b81edd4d0a8d0e02
Olga Arkhipenko <orangeorca909@gmail.com>
2018-06-07 15:04:21 +0300

    Created special user block to display logged in  user data.

---
+++ client/modules/user/components/UserBlock.vue
@@ -0,0 +1,27 @@
+<template>
+    <section>
+        <p class="user-name">
+            hello
+        </p>
+        <div class="user-avatar">
+            <img
+                src=""
+                alt="User avatar.">
+        </div>
+        <button class="sigh-out-cutton">Sign out</button>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    computed: {
+        isAuthorized()
+    }
+}
+</script>
+
