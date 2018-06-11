9a48411eb84a2488982310e3ea70b4cb6aa4998e
Olga Arkhipenko <orangeorca909@gmail.com>
2018-06-11 19:48:32 +0300

    Fixed data layer user mapper.

--- server/dataAccess/mappers/userMapper.js
+++ server/dataAccess/mappers/userMapper.js
@@ -5,9 +5,11 @@ const imageMapper = require('./imageMapper');
 const mapUser = function (databaseModel, TargetType) {
     const mappedUser = new TargetType();
     mappedUser.id = databaseModel.dataValues.id;
+    mappedUser.name = databaseModel.dataValues.name;
     mappedUser.email = databaseModel.dataValues.email;
     mappedUser.birthdate = databaseModel.dataValues.birthdate;
-    mappedUser.name = databaseModel.dataValues.name;
+    mappedUser.password = databaseModel.dataValues.password;
+    mappedUser.salt = databaseModel.dataValues.salt;
 
     return mappedUser;
 };
