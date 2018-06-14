0216e34aaa403fb870137f4f8d0bfbe55e4d30bd
Olga Arkhipenko <orangeorca909@gmail.com>
2018-06-14 11:03:07 +0300

    Modified user block styles.

--- client/modules/user/components/UserBlock.vue
+++ client/modules/user/components/UserBlock.vue
@@ -3,14 +3,14 @@
         <p
             v-cloak
             class="user-block__name">
-            {{ name }}
+            Hello, {{ name }}
         </p>
         <img
             :src="profilePictureUrl"
             alt="User avatar."
             class="user-block__avatar">
         <button
-            class="sigh-out-button"
+            class="sign-out-button"
             @click="submitSignOut">
             Sign out
         </button>
@@ -42,10 +42,25 @@ export default {
 .user-block {
     display: flex;
     align-items: center;
+    margin: 0 20px 0;
+    font-family: "Courier New", Courier, monospace;
+    font-size: 1.6rem;
+}
+
+.user-block__name {
+    margin: 0 20px 0;
 }
 
 .user-block__avatar {
     max-height: 80px;
+}
+
+.sign-out-button {
+    cursor: pointer;
+    text-transform: uppercase;
+    color: #38a39e;
+    border: none;
+    background-color: transparent;
 }
 </style>
 
