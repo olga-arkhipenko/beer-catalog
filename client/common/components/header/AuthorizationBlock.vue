dd206335a577160f013f060bcc98f76b58465ffb
Olga Arkhipenko <orangeorca909@gmail.com>
2018-06-14 11:02:50 +0300

    Scoped authorization block styles in authorization component.

--- client/common/components/header/AuthorizationBlock.vue
+++ client/common/components/header/AuthorizationBlock.vue
@@ -12,3 +12,25 @@
         </router-link>
     </div>
 </template>
+
+<style>
+.authorization {
+    margin: 0 30px 0 0;
+}
+
+.authorization__signup-button {
+    color: #ffb259;
+    text-shadow: #000000 1px 1px 0, #000000 -1px -1px 0, #000000 -1px 1px 0, #000000 1px -1px 0;
+
+    font-family: "Courier New", Courier, monospace;
+    font-size: 1.6rem;
+}
+
+.authorization__login-button {
+    color: #40ca3a;
+
+    font-family: "Courier New", Courier, monospace;
+    font-size: 1.6rem;
+}
+</style>
+
