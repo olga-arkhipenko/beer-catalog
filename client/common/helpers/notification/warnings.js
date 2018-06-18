06c5f860fe28af75a3ba4a86984b2717f5f5eab4
Olga Arkhipenko <orangeorca909@gmail.com>
2018-06-18 16:12:18 +0300

    Modified notification message.

--- client/common/helpers/notification/warnings.js
+++ client/common/helpers/notification/warnings.js
@@ -1,4 +1,4 @@
 export default {
     authorization: 'You are not logged in and cannot add favorite beers. Please, login or sign up',
-    signout: 'You have been sign out. Now you cannot manage favorite beers.'
+    signout: 'Something went wrong... You cannot sign out now.'
 };
