9cb0e75acd99a81c232d009a233cd97afcb65559
Olga Arkhipenko <orangeorca909@gmail.com>
2018-06-19 14:02:59 +0300

    Added notification timeout.

--- client/common/helpers/notification/notificationHelper.js
+++ client/common/helpers/notification/notificationHelper.js
@@ -6,6 +6,7 @@ export default {
             type: 'error',
             theme: 'sunset',
             layout: 'topCenter',
+            timeout: 3000,
             text: errorMessage
         }).show();
     },
@@ -14,6 +15,7 @@ export default {
             type: 'warning',
             theme: 'sunset',
             layout: 'topRight',
+            timeout: 3000,
             text: warningMessage
         }).show();
     },
@@ -22,6 +24,7 @@ export default {
             type: 'success',
             theme: 'sunset',
             layout: 'topCenter',
+            timeout: 3000,
             text: successMessage
         }).show();
     }
