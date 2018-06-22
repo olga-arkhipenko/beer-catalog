8eff690ce5f41edfbf05166b01a08342fdba5e2d
Olga Arkhipenko <volha.arkhipenka@itechart-group.com>
2018-06-22 15:37:09 +0300

    Replaced request helper implementation with node-fetch usage.

--- server/helpers/requestHelper.js
+++ server/helpers/requestHelper.js
@@ -1,17 +1,8 @@
-const https = require('https');
+const fetch = require('node-fetch');
 
 module.exports = {
     get(url) {
-        return new Promise((resolve, reject) => {
-            https.get(url, (res) => {
-                if (res.statusCode < 200 && res.statusCode > 299) {
-                    reject(res.statusCode);
-                }
-                let json = '';
-                res.on('data', (chunk) => { json += chunk; });
-                res.on('end', () => resolve(JSON.parse(json)));
-            }).on('error', err => reject(err));
-        });
+        return fetch(url)
+            .then(result => result.json());
     }
 };
-
