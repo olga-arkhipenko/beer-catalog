0c3a3432a26a2b6648cc941b0703f2e34a61cb5c
Olga Arkhipenko <volha.arkhipenka@itechart-group.com>
2018-06-25 11:57:40 +0300

    Deleted redundant promises.

--- server/dataAccess/repositories/favoritesRepository.js
+++ server/dataAccess/repositories/favoritesRepository.js
@@ -12,49 +12,43 @@ module.exports = {
             .then(favorites => favorites.map(favorite => favorite.beerId));
     },
     addFavorite(beerId, userId) {
-        return new Promise((resolve, reject) => {
-            database.user.findById(userId)
-                .then((user) => {
-                    if (user) {
-                        resolve(user);
-                    } else {
-                        reject(new Error('User not found.'));
-                    }
-                })
-                .catch(err => reject(err));
-        }).then(user => database.favorite
-            .findOrCreate({
-                where: {
-                    beerId
-                },
-                include: database.user
+        return database.user.findById(userId)
+            .then((user) => {
+                if (user) {
+                    return user;
+                }
+                throw new Error('User not found.');
             })
-            .then((beers) => {
-                user.addFavorite(beers[0]);
-                return beers[0].beerId;
-            }));
+            .then(user => database.favorite
+                .findOrCreate({
+                    where: {
+                        beerId
+                    },
+                    include: database.user
+                })
+                .then((beers) => {
+                    user.addFavorite(beers[0]);
+                    return beers[0].beerId;
+                }));
     },
     removeFavorite(beerId, userId) {
-        return new Promise((resolve, reject) => {
-            database.user.findById(userId)
-                .then((user) => {
-                    if (user) {
-                        resolve(user);
-                    } else {
-                        reject(new Error('User not found.'));
-                    }
-                })
-                .catch(err => reject(err));
-        }).then(user => database.favorite
-            .findOne({
-                where: {
-                    beerId
-                },
-                include: database.user
+        return database.user.findById(userId)
+            .then((user) => {
+                if (user) {
+                    return user;
+                }
+                throw new Error('User not found.');
             })
-            .then((beer) => {
-                user.removeFavorite(beer);
-                return beer.beerId;
-            }));
+            .then(user => database.favorite
+                .findOne({
+                    where: {
+                        beerId
+                    },
+                    include: database.user
+                })
+                .then((beer) => {
+                    user.removeFavorite(beer);
+                    return beer.beerId;
+                }));
     }
 };
