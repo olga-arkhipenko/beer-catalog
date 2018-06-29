af3b7ca57497b28f1bfeab97105415df9e8c9ced
Olga Arkhipenko <volha.arkhipenka@itechart-group.com>
2018-06-29 16:44:22 +0300

    Created pluggable notification helper.

--- client/common/helpers/notification/notificationHelper.js
+++ client/common/helpers/notification/notificationHelper.js
@@ -1,14 +1,35 @@
 import Noty from 'noty';
 
+class NotificationManager {
+    constructor(promise) {
+        this.promise = promise;
+    }
+
+    createNotification(config) {
+        return this.promise
+            .then(() =>
+                new Noty({
+                    type: config.success.type,
+                    theme: 'sunset',
+                    layout: 'topCenter',
+                    timeout: 3000,
+                    text: config.success.message
+                }).show())
+            .catch(() =>
+                new Noty({
+                    type: config.error.type,
+                    theme: 'sunset',
+                    layout: 'topCenter',
+                    timeout: 3000,
+                    text: config.error.message
+                }).show());
+    }
+}
+
 export default {
-    showNotification(type, message) {
-        new Noty({
-            type,
-            theme: 'sunset',
-            layout: 'topCenter',
-            timeout: 3000,
-            text: message
-        }).show();
+    showNotification(promise, config) {
+        const notificationManager = new NotificationManager(promise);
+        return notificationManager.createNotification(config);
     }
 };
 
