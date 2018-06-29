ce34e17faca25bee9c915dc6d7e2cb0c4a382bdb
Olga Arkhipenko <volha.arkhipenka@itechart-group.com>
2018-06-29 16:42:49 +0300

    Created common notification config.

---
+++ client/common/helpers/notification/configs.js
@@ -0,0 +1,42 @@
+export default {
+    registration: {
+        success: {
+            type: 'success',
+            message: 'You have been registered successfully!'
+        },
+        error: {
+            type: 'error',
+            message: 'Registration failed.'
+        }
+    },
+    login: {
+        error: {
+            type: 'error',
+            message: 'Login failed.'
+        }
+    },
+    beersLoading: {
+        error: {
+            type: 'error',
+            message: 'Cannot load beers, sorry.'
+        }
+    },
+    beerLoading: {
+        error: {
+            type: 'error',
+            message: 'Cannot load beer, sorry.'
+        }
+    },
+    authorization: {
+        warning: {
+            type: 'warning',
+            message: 'You are not logged in and cannot add favorite beers. Please, log in or sign up.'
+        }
+    },
+    signout: {
+        warning: {
+            type: 'warning',
+            message: 'Something went wrong... You cannot sign out now.'
+        }
+    }
+};
