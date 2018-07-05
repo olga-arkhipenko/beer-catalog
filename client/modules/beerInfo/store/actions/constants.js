871d893a9c485ef9e6738c140a43afd7a8bb6016
Olga Arkhipenko <orangeorca909@gmail.com>
2018-07-05 23:33:50 +0300

    Added module name to  action constants names.

--- client/modules/beerInfo/store/actions/constants.js
+++ client/modules/beerInfo/store/actions/constants.js
@@ -1,7 +1,7 @@
-const LOAD_BEER = 'beerInfo-loadBeer';
-const RESET_BEER = 'beerInfo-resetBeer';
-const ADD_FAVORITE_BEER = 'beerInfo-addFavoriteBeer';
-const REMOVE_FAVORITE_BEER = 'beerInfo-removeFavoriteBeer';
+const LOAD_BEER = 'beerInfo/loadBeer';
+const RESET_BEER = 'beerInfo/resetBeer';
+const ADD_FAVORITE_BEER = 'beerInfo/addFavoriteBeer';
+const REMOVE_FAVORITE_BEER = 'beerInfo/removeFavoriteBeer';
 
 export default {
     LOAD_BEER,
