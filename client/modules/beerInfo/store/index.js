47a897b2476accf397eb3078c840bff2bf5df195
Olga Arkhipenko <orangeorca909@gmail.com>
2018-07-05 23:38:12 +0300

    Separated state contants and object into files.

--- client/modules/beerInfo/store/index.js
+++ client/modules/beerInfo/store/index.js
@@ -1,11 +1,9 @@
 import mutations from './mutations/index';
 import actions from './actions/index';
+import state from './state/index';
 
 export default {
-    // namespaced: true,
-    state: {
-        beer: {}
-    },
+    state,
     mutations,
     actions
 };
