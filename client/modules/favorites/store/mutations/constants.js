4126bfe9bcde74d53a35f4c006b6b5251f3ac893
olga-arkhipenko <orangeorca909@gmail.com>
2018-07-06 14:55:49 +0300

    Added namespace prefix to mutations constants.

--- client/modules/favorites/store/mutations/constants.js
+++ client/modules/favorites/store/mutations/constants.js
@@ -1,6 +1,6 @@
-const SET_FAVORITE_BEERS = 'setFavoriteBeers';
-const SET_AMOUNT_OF_PAGES = 'setAmountOfPages';
-const RESET_BEERS = 'resetBeers';
+const SET_FAVORITE_BEERS = 'favorites/setFavoriteBeers';
+const SET_AMOUNT_OF_PAGES = 'favorites/setAmountOfPages';
+const RESET_BEERS = 'favorites/resetBeers';
 
 export default {
     SET_FAVORITE_BEERS,
