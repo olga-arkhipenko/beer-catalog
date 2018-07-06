961f0a41b500cbacdea7cba8cc1e06b1577a1679
olga-arkhipenko <orangeorca909@gmail.com>
2018-07-06 16:46:44 +0300

    Modififed mutation names in user module.

--- client/modules/user/store/mutations/constants.js
+++ client/modules/user/store/mutations/constants.js
@@ -1,5 +1,5 @@
-const SET_USER_DATA = 'setUserData';
-const RESET_USER_DATA = 'resetUserData';
+const SET_USER_DATA = 'userData/setUserData';
+const RESET_USER_DATA = 'userData/resetUserData';
 
 export default {
     SET_USER_DATA,
