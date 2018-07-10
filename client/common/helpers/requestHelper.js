1bd6bb5d099db1fb8588e41e64bb2a0c73d399ab
olga-arkhipenko <orangeorca909@gmail.com>
2018-07-10 12:19:34 +0300

    Left request helper as it is.

--- client/common/helpers/requestHelper.js
+++ client/common/helpers/requestHelper.js
@@ -1,14 +1,22 @@
 export default {
     sendRequest(url, method = 'get', data) {
-        return fetch(url, {
-            method,
-            //     headers: {
-            //         Accept: 'application/json',
-            //         'Content-Type': 'application/json'
-            //     },
-            //     credentials: 'include',
-            body: data
-        });
+        return data ?
+            fetch(url, {
+                method,
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                credentials: 'include',
+                body: data ? JSON.stringify(data) : ''
+            })
+            : fetch(url, {
+                method,
+                headers: {
+                    Accept: 'application/json'
+                },
+                credentials: 'include'
+            });
     }
 };
 
