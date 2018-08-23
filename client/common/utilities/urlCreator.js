ee82b537806598d8b5c5ebc5371a06ad2a6460b7
Olga Arkhipenko <orangeorca909@gmail.com>
2018-08-23 19:36:02 +0300

    Refactored url creator.

--- client/common/utilities/urlCreator.js
+++ client/common/utilities/urlCreator.js
@@ -1,13 +1,13 @@
+import UrlAssembler from 'url-assembler';
+
 export default {
-    create(route, params) {
-        const queryString =
-        Object.keys(params)
-            .reduce((accumulator, param) => {
-                accumulator.push(`${param}=${params[param]}`);
-                return accumulator;
-            }, [])
-            .join('&');
-        return `${route}?${queryString}`;
+    createUrl(prefix, template, params, query) {
+        return new UrlAssembler()
+            .prefix(prefix || '')
+            .template(template)
+            .param(params)
+            .query(query)
+            .toString();
     }
 };
 
