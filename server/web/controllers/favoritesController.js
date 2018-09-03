dfcba225034aa3721f6c46cb2ded11494f6372c4
Olga Arkhipenko <orangeorca909@gmail.com>
2018-09-03 14:57:49 +0300

    Refactored favorites controller.

--- server/web/controllers/favoritesController.js
+++ server/web/controllers/favoritesController.js
@@ -4,47 +4,55 @@ const cookieParams = require('../configs/cookieParams');
 const responseHandler = require('../handler/responseHandler');
 
 module.exports = {
-    addFavorite(req, res) {
-        const token = req.cookies[cookieParams.name];
-        const beerId = req.body.beerId;
-        if (token) {
-            const promise = jwtHelper.verifyToken(token)
-                .then(decodedToken => favoritesService.addFavorite(beerId, decodedToken.id))
-                .then(favoriteBeerId =>
-                    responseHandler.sendResponse(res, 200, { beerId: favoriteBeerId }));
-            promise.catch(err => responseHandler.sendResponse(res, 500, err));
-        } else {
-            responseHandler.sendResponse(res, 401);
+    async addFavorite(request, response) {
+        const token = request.cookies[cookieParams.name];
+        const beerId = request.body.beerId;
+        let decodedToken = null;
+        try {
+            decodedToken = await jwtHelper.verifyToken(token);
+        } catch (err) {
+            responseHandler.sendResponse(response, 401, err);
+            return;
+        }
+        try {
+            const favoriteBeerId = await favoritesService.addFavorite(beerId, decodedToken.id);
+            responseHandler.sendResponse(response, 200, { beerId: favoriteBeerId });
+        } catch (err) {
+            responseHandler.sendResponse(response, 500, err);
         }
     },
-    removeFavorite(req, res) {
-        const token = req.cookies[cookieParams.name];
-        const beerId = req.body.beerId;
-        if (token) {
-            const promise = jwtHelper.verifyToken(token)
-                .then(decodedToken => favoritesService.removeFavorite(beerId, decodedToken.id))
-                .then(unfavoriteBeerId =>
-                    responseHandler.sendResponse(res, 200, { beerId: unfavoriteBeerId }));
-            promise.catch(err => responseHandler.sendResponse(res, 401, err));
-        } else {
-            responseHandler.sendResponse(res, 401);
+    async removeFavorite(request, response) {
+        const token = request.cookies[cookieParams.name];
+        const beerId = request.body.beerId;
+        let decodedToken = null;
+        try {
+            decodedToken = await jwtHelper.verifyToken(token);
+        } catch (err) {
+            responseHandler.sendResponse(response, 401, err);
+            return;
+        }
+        try {
+            const unfavoriteBeerId = favoritesService.removeFavorite(beerId, decodedToken.id);
+            responseHandler.sendResponse(response, 200, { beerId: unfavoriteBeerId });
+        } catch (err) {
+            responseHandler.sendResponse(response, 500, err);
         }
     },
-    getFavorites(req, res) {
-        const token = req.cookies[cookieParams.name];
-        const query = req.query;
-        if (token) {
-            const promise = jwtHelper.verifyToken(token)
-                .then((decodedToken) => {
-                    favoritesService
-                        .getFavorites(decodedToken.id, query)
-                        .then((favoritesData) => {
-                            responseHandler.sendResponse(res, 200, favoritesData);
-                        });
-                });
-            promise.catch(err => responseHandler.sendResponse(res, 401, err));
-        } else {
-            responseHandler.sendResponse(res, 401);
+    async getFavorites(request, response) {
+        const token = request.cookies[cookieParams.name];
+        const query = request.query;
+        let decodedToken = null;
+        try {
+            decodedToken = await jwtHelper.verifyToken(token);
+        } catch (err) {
+            responseHandler.sendResponse(response, 401, err);
+            return;
+        }
+        try {
+            const favoritesData = await favoritesService.getFavorites(decodedToken.id, query);
+            responseHandler.sendResponse(response, 200, favoritesData);
+        } catch (err) {
+            responseHandler.sendResponse(response, 500, err);
         }
     }
 };
