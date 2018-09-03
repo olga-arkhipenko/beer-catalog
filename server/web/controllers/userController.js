bc146436f1ea3b91863fffbe3db548a567df6ca1
Olga Arkhipenko <orangeorca909@gmail.com>
2018-09-03 10:46:40 +0300

    Refactored.

--- server/web/controllers/userController.js
+++ server/web/controllers/userController.js
@@ -9,21 +9,23 @@ module.exports = {
     async register(request, response) {
         const registrationData = userMapper.mapToRegistrationData(request.body);
         try {
-            const userData = await userService.register(registrationData);
-            const user = userMapper.mapToUser(userData);
+            const user = await userService
+                .register(registrationData)
+                .then(userData => userMapper.mapToUser(userData));
             responseHandler.sendResponse(response, 200, user);
         } catch (err) {
             responseHandler.sendResponse(response, 500, err);
         }
     },
-    uploadProfilePicture(req, res) {
-        userService
-            .uploadProfilePicture(req.body)
-            .then((image) => {
-                const mappedImage = imageMapper.mapToImage(image);
-                responseHandler.sendResponse(res, 200, mappedImage);
-            })
-            .catch(err => responseHandler.sendResponse(res, 500, err));
+    async uploadProfilePicture(request, response) {
+        try {
+            const image = await userService
+                .uploadProfilePicture(request.body)
+                .then(imageData => imageMapper.mapToImage(imageData));
+            responseHandler.sendResponse(response, 200, image);
+        } catch (err) {
+            responseHandler.sendResponse(response, 500, err);
+        }
     },
     login(req, res) {
         userService
