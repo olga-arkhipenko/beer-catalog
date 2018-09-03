7ed0f7cde15f274784e1f47cc6700c7b80f0345c
Olga Arkhipenko <orangeorca909@gmail.com>
2018-09-03 20:11:09 +0300

    Created userData model.

---
+++ server/business/models/userData.js
@@ -0,0 +1,10 @@
+module.exports = class UserData {
+    constructor() {
+        this.name = null;
+        this.email = null;
+        this.birthdate = null;
+        this.password = null;
+        this.salt = null;
+        this.profilePictureId = null;
+    }
+};
