0b428fecfd036b30e913a2b0fb5141f8934279b2
Olga Arkhipenko <orangeorca909@gmail.com>
2018-09-03 10:36:33 +0300

    Created registration data model.

---
+++ server/web/models/registrationData.js
@@ -0,0 +1,10 @@
+module.exports = class RegistrationData {
+    constructor() {
+        this.name = null;
+        this.mail = null;
+        this.birthdate = null;
+        this.password = null;
+        this.profilePicture = null;
+    }
+};
+
