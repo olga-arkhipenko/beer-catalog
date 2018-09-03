b01ba840e5ac1bd33d1812c53b2c071b28465195
Olga Arkhipenko <orangeorca909@gmail.com>
2018-09-03 10:03:53 +0300

    Refactored.

--- server/web/managers/favoritesManager.js
+++ server/web/managers/favoritesManager.js
@@ -3,11 +3,10 @@ const favoritesService = require('../../business/services/favoritesService');
 module.exports = {
     async markBeersAsFavorite(beers, userId) {
         const favoritesIds = await favoritesService.getFavoritesIds(userId);
-        const res = beers.map((beer) => {
+        return beers.map((beer) => {
             beer.isFavorite = !!favoritesIds.includes(beer.id);
             return beer;
         });
-        return res;
     }
 };
 
